6241fd8d54ee1fdbe4012c36acc1e277584ab256
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-06 21:02:37 +0300

    Task 1 completed

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -2,19 +2,21 @@ import $ from "jquery";
 import debounce from 'lodash/debounce';
 
 
-$.(document).ready( function() {
+$(document).ready(function() {
   paragraph1 = $('<p>').text('Holberton Dashboard');
   paragraph2 = $('<p>').text('Dashboard data for the students');
   button = $('<button>').text('Click here to get started');
   paragraph3 = $('<p id="count">').text('')
   paragraph4 = $('<p>').text('Copyright - Holberton School')
+
+  let count = 0;
+  function updateCounter() {
+    count++;
+
+    paragraph3.text(`${count} clicks on the button)`);
+  }
+
+  button.addEventListener('click', debounce(updateCounter()));
+
+  $(body).append(paragraph1, paragraph2, button, paragraph3, paragraph4);
 });
-
-let count = 0;
-function updateCounter() {
-  count++;
-
-  paragraph3.text(`${count} clicks on the button)`);
-}
-
-button.addEventListener('click', debounce(updateCounter());
