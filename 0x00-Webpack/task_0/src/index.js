1705f8a40623ea75acb1c0c6523b8775575217eb
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-06 19:58:01 +0300

    Syntax fix

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,9 +1,9 @@
 import $ from "jquery";
 
 $(document).ready(function() {
-  var paragraph1 = $('<p>').text('Holberton Dashboard'));
-  var paragraph2 = $('<p>').text('Dashboard data for the students'));
-  var paragraph3 = $('<p>').text('Holberton Dashboard'));
+  var paragraph1 = $('<p>').text('Holberton Dashboard');
+  var paragraph2 = $('<p>').text('Dashboard data for the students');
+  var paragraph3 = $('<p>').text('Holberton Dashboard');
 
   $('body').append(paragraph1, paragraph2, paragraph3);
-)
+});
