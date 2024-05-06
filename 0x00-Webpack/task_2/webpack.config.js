f3b5cf6cf77f490afca4c38b21a8b33303538b95
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-06 21:35:23 +0300

    Task 2

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  mode: "production",
+
+  module: {
+    rules: [
+      {
+        test: /\.css/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        use: ['file-laoder'],
+      },
+    ]
+  },
+};
