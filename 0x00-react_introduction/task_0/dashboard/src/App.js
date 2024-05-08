4b48a41061f5550c08d289156b6391d558d48fb2
BENONI ESCKINDER <you@example.com>
2024-05-09 01:30:28 +0300

    I don't really get it but I'll do it anyway

---
+++ 0x00-react_introduction/task_0/dashboard/src/App.js
@@ -0,0 +1,26 @@
+import logo from './holberton-logo.jpg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="App-header">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1>School dashboard</h1>
+        </header>
+        <hr />
+      </div>
+
+      <div className="App-body">
+          <p>Login to access the full dashboard</p>
+      </div>
+
+      <div className="App-footer">
+        <p>Copyright 2020 - hoberton School</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
