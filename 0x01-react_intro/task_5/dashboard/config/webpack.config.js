896372470001fe6b5be5b00f5c318b43570bb660
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-09 12:43:50 +0300

    Fixing task 9

--- 0x01-react_intro/task_5/dashboard/config/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/config/webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
             test: /\.(png|jpe?g|gif)$/i,
             use: [
               {
-                loader: 'file-loader',
+                loader: 'image-webpack-loader',
               },
             ],
           },
