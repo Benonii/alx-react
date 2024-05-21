95b928d95209cd537d6327ddd463cbb4e72f4860
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-21 22:37:39 +0300

    Inline styling with aphroditie

---
+++ 0x04-React_inline_styling/task_1/dashboard/src/BodySection/BodySectionWithMarginBottom.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import BodySectioWithnMarginBottom from "./BodySectionWithMarginBottom";
+import BodySection from "./BodySection";
+
+describe('<BodySectionWithBottomMargin', () => {
+    it('renders a BodySection element', () => {
+        const wrapper = shallow(
+        <BodySectioWithnMarginBottom title='test'>
+            <p>Testing</p>
+        </BodySectioWithnMarginBottom>);
+
+        expect(wrapper.find(BodySection).length).toBe(1);
+        const component = wrapper.props().children;
+        console.log(component.props)
+        expect(component.props.title).toBe('test');
+        expect(component.props.children).toStrictEqual(<p>Testing</p>);
+    });
+});
---
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,131 @@
+import React, { Component } from "react";
+import { StyleSheet, css} from "aphrodite";
+import img from "../assets/close-icon.png"
+import NotificationItem from "./NotificationItem.js";
+import NotificationItemShape from "./NotificationItemShape.js";
+import propTypes from "prop-types";
+
+export default class Notifications extends Component {
+  constructor(props) {
+    super(props)
+  //   this.state = {
+  //     listNotifications: this.props.listNotifications,
+  //   }
+  }
+
+  static defaultProps = {
+    displayDrawer: false,
+    listNotifications: [],
+  }
+
+  markAsRead(id) {
+    console.log(`Notification ${id} has been marked as read`);
+  }
+
+  // shouldComponentUpdate() {
+  //   if (this.props.listNotifications > this.state.listNotifications) {
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // };
+
+  render() {
+    const { displayDrawer, listNotifications } = this.props;
+    if (listNotifications.length === 0) {
+      return (
+        <div className='Notifications'>
+          <div className="menuItem">
+            <p>Your notifications</p>
+          </div>
+          {displayDrawer && ( 
+            <div className='Notifications-content'>
+              <tr><td>No new notifications for now</td></tr>
+            </div>)}
+          </div>
+      )
+    } else {
+      return (
+          <>
+          <div className='Notifications'>
+            <div className={css(styles.menuItem)}>
+              <p>Your notifications</p>
+            </div>
+            {displayDrawer && ( 
+              <div className={css(styles.notificationsContent)}>
+                <p>
+                    Here is the list of notifications
+                </p>
+                <ul>
+                  {this.props.listNotifications.map((notification) => {
+                    return (
+                      <NotificationItem key={notification.id}
+                                        type={notification.type}
+                                        value={notification.value}
+                                        html={notification.html}
+                                        markAsRead={() => {this.markAsRead(notification.id)}}/>
+                    )
+                  })};    
+                </ul>
+
+                <button
+                  className={css(styles.button)}
+                  style = {{right: '0'}}
+                  aria-label="Close"
+                  onClick={() => {console.log('Close button has been clicked')}}>
+                    <img className={css(styles.buttonImg)} src={img} alt="close icon" />
+                  </button>
+                </div>
+            )}
+          </div>
+          </>
+      )
+    }
+  }
+}
+
+
+Notifications.propTypes = {
+  displayDrawer: propTypes.bool,
+  listNotifications: propTypes.arrayOf(NotificationItemShape)
+};
+
+const styles = StyleSheet.create({
+  menuItem: {
+    textAlign: 'right',
+    marginRight: 10,
+    fontSize: '120%',
+    fontWeight: 500,
+  },
+
+  notificationsContent: {
+    position: 'relative',
+    border: '1px dashed red',
+    padding: '10px 50px 10px 50px',
+  },
+
+  button: {
+      marginTop: 10,
+      marginRight: 15,
+      position: 'absolute',
+      backgroundColor: 'transparent',
+      border: 'none',
+      width: 10,
+      height: 10,
+      top: 0,
+      right: 0,
+  },
+
+  buttonImg: {
+      width: 10,
+  },
+
+  defaultNotification: {
+      color: 'blue',
+  },
+    
+  urgentNotifcation: {
+      color: 'red',
+  }
+  
+})
