aefbccfe2852753702414ce6b826cf7e5e49cf07
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-22 00:04:44 +0300

    Deleted login.css

--- 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Notifications/NotificationItem.js
@@ -1,22 +1,21 @@
 import React from "react";
-import "./NotificationItem.css"
+import { StyleSheet, css } from "aphrodite";
+import "./NotificationItem.css";
 import propTypes from "prop-types";
 import "./Notifications.css";
 
 export default function NotificationItem(props) {
-    // const [state, setState] = React.useState(props);
-
-    // if (props !== state) {
-        
-    // }
     let list_item;
     props.html ? list_item = (
-        <li data-notification-type={props.type}
+
+        <li
+            data-notification-type={props.type}
             dangerouslySetInnerHTML={{ __html: props.html }}
             onClick={props.markAsRead}>
             </li>
     ) : list_item = (
-        <li data-notification-type={props.type} onClick={props.markAsRead}>{props.value}</li>
+        <li
+            data-notification-type={props.type} onClick={props.markAsRead}>{props.value}</li>
     )
     return list_item
 }
