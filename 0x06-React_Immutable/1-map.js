a58b92e8a7d62e6629e7cedf31c7761b5c8821f3
BENONI ESCKINDER <you@example.com>
2024-05-27 22:07:31 +0300

    Removing examples + task 4

--- 0x06-React_Immutable/1-map.js
+++ 0x06-React_Immutable/1-map.js
@@ -3,12 +3,3 @@ const { Map } = require('immutable');
 function getImmutableObject (object) {
   return Map(object);
 }
-
-const obj = {
-  fear: true,
-  smell: -1033575916.9145899,
-  wall: false,
-  thing: -914767132
-};
-
-console.log(getImmutableObject(obj));
--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -2,10 +2,3 @@
 export default function accessImmutableObject (object, array) {
   return array.reduce((acc, key) => acc && acc[key], object);
 }
-
-console.log(accessImmutableObject({
-  name: {
-    first: 'Guillaume',
-    last: 'Salva'
-  }
-}, ['name', 'first']));
