355847ef1bec23984ea35652a8c3357bbcd0aa1d
BENONI ESCKINDER <you@example.com>
2024-05-27 21:20:08 +0300

    README + task 0

---
+++ 0x06-React_Immutable/0-fromjs.js
@@ -0,0 +1,14 @@
+const { fromJS } = require('immutable');
+
+function getImmutableObject (object) {
+  return fromJS(object);
+}
+
+const obj = {
+  fear: true,
+  smell: -1033575916.9145899,
+  wall: false,
+  thing: -914767132
+};
+
+console.log(getImmutableObject(obj));
