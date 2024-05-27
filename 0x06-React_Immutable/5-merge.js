6d730b33ea500506c97dcbf2f8c7b320244b14e4
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-27 22:52:18 +0300

    Task 6

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,11 @@
+const { Map, List } = require('immutable');
+
+export function concatElements (page1, page2) {
+  return List(page1).concat(page2);
+}
+
+export function mergeElements (page1, page2) {
+  const map1 = Map(page1);
+  const map2 = Map(page2);
+  return List((map1.merge(map2)).values());
+}
---
+++ 0x06-React_Immutable/6-deeply.js
@@ -0,0 +1,34 @@
+const {List, Map } = require('immutable');
+
+function mergeDeeplyElements(page1, page2) {
+    const map1 = Map(page1);
+    const map2 = Map(page2);
+
+    const deepMerge = map1.mergeDeep(map2);
+    return List(deepMerge.values());
+}
+
+// const page1 = {
+//     'user-1': {
+//       id: 1,
+//       name: 'test',
+//       likes: {
+//         1: {
+//           uid: 1234,
+//         }
+//       }
+//     },
+//   };
+  
+//   const page2 = {
+//     'user-1': {
+//       likes: {
+//         2: {
+//           uid: 134,
+//         }
+//       }
+//     },
+//   };
+
+//   result = mergeDeeplyElements(page1, page2);
+//   console.log(result.toJS());
