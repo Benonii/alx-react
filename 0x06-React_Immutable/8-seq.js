5f93906c2ec782b718c627c12c21ab0450c50ad8
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-28 14:46:58 +0300

    Pushing

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,12 @@
+const { Seq } = require('immutable');
+
+export default function printBestStudents (grades) {
+  Seq(grades)
+    .filter((student) => student.score >= 70)
+    .map((student) => ({
+      ...student,
+      firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
+      lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1)
+    }))
+    .forEach((student) => console.log(JSON.stringify({ [student.id]: student })));
+}
