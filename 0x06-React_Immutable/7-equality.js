1019a4fceb21095947b1a5220421686012500cf1
BENONI ESCKINDER <you@example.com>
2024-05-28 16:55:15 +0300

    ESlint fix

--- 0x06-React_Immutable/7-equality.js
+++ 0x06-React_Immutable/7-equality.js
@@ -1,5 +1,5 @@
-const { is } = require('immutable');
+import { is } from 'immutable';
 
-export default function areMapsEqual (map1, map2) {
+export default function areMapsEqual(map1, map2) {
   return is(map1, map2);
 }
