df297c1844264398e1f822c1346fdcbdddb56991
BENONI ESCKINDER <you@example.com>
2024-05-28 20:20:30 +0300

    Atoring merge in a variable

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -7,5 +7,6 @@ export function concatElements(page1, page2) {
 export function mergeElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
-  return List((map1.merge(map2)));
+  const mergedMap = map1.merge(map2);
+  return List((mergedMap.values()));
 }
