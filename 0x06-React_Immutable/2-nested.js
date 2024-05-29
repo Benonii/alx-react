d3076aaaa1d5d9abcc2cd57a6e13ba4a4d680908
BENONI ESCKINDER <you@example.com>
2024-05-29 05:48:08 +0300

    Accessing the immutable differently

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -2,5 +2,5 @@ import { Map } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
   const map = Map(object);
-  return array.reduce((acc, key) => acc && acc[key], map.toJS());
+  return array.reduce((acc, key) => acc && acc.get(key), map);
 }
