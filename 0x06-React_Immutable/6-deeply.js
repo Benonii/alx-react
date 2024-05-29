15ed2b7f8c49b50bc178fddc16a82bb7b4c17d8a
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-29 08:18:30 +0300

    Using a different way to create the list

--- 0x06-React_Immutable/6-deeply.js
+++ 0x06-React_Immutable/6-deeply.js
@@ -1,9 +1,9 @@
-import { List, Map } from 'immutable';
+import { Map } from 'immutable';
 
 export default function mergeDeeplyElements(page1, page2) {
   const map1 = Map(page1);
   const map2 = Map(page2);
 
   const deepMerge = map1.mergeDeep(map2);
-  return List(deepMerge.values());
+  return deepMerge.toList();
 }
