b2ac1a690c0f61499ac0b1edd9704679b686e9ec
BENONI ESCKINDER <you@example.com>
2024-05-29 07:29:03 +0300

    Changing export methods back

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
 import { Map } from 'immutable';
 
-const map = Map({
+export const map = Map({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,5 +9,4 @@ const map = Map({
   6: 'Lucas',
 });
 
-const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
-module.exports = [map, map2];
+export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
