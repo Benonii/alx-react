54f8617af3b8e8cbf8a127ec820dd869acec2625
BENONI ESCKINDER <you@example.com>
2024-05-29 06:13:06 +0300

    Adding export + removing example

--- 0x06-React_Immutable/2-nested.js
+++ 0x06-React_Immutable/2-nested.js
@@ -1,13 +1,6 @@
 import { fromJS } from 'immutable';
 
-function accessImmutableObject(object, array) {
+export default function accessImmutableObject(object, array) {
   const map = fromJS(object);
   return array.reduce((acc, key) => acc && acc.get(key), map);
 }
-
-console.log(accessImmutableObject({
-  name: {
-       first: "Guillaume",
-       last: "Salva"
-  }
-}, ['name', 'first']))
