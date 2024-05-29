16e5ce449dc805a9ed7f36aabb5671ef94f82a71
BENONI ESCKINDER <you@example.com>
2024-05-29 08:44:55 +0300

    Fixing the console log

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,12 +1,14 @@
 import { Seq } from 'immutable';
 
 export default function printBestStudents(grades) {
-  Seq(grades)
+  const gradesSeq = Seq(grades);
+  gradesSeq
     .filter((student) => student.score >= 70)
     .map((student) => ({
       ...student,
       firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
-    }))
-    .forEach((student) => console.log(JSON.stringify({ [student.id]: student })));
+    }));
+
+  console.log(gradesSeq.toJS());
 }
