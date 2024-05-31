c80f926cc4ac785fa28b36a3da0a7a5ffd921f31
BENONI ESCKINDER <you@example.com>
2024-05-31 05:35:42 +0300

    Using fromJS instead of Map

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -1,6 +1,6 @@
-import { Map } from 'immutable';
+import { fromJS } from 'immutable';
 
-export const map = Map({
+export const map = fromJS({
   1: 'Liam',
   2: 'Noah',
   3: 'Elijah',
@@ -9,4 +9,3 @@ export const map = Map({
   6: 'Lucas',
 });
 export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
- 
