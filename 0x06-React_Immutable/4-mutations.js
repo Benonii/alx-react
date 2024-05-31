f380fcfc17d08adf906d1b676281da4bbec57fb2
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-31 11:48:31 +0300

    Using withMutations

--- 0x06-React_Immutable/4-mutations.js
+++ 0x06-React_Immutable/4-mutations.js
@@ -8,4 +8,6 @@ export const map = fromJS({
   5: 'Jacob',
   6: 'Lucas',
 });
-export const map2 = map.set(2, 'Benjamin').set(4, 'Oliver');
+export const map2 = map.withMutations((temp) => {
+  temp.set(2, 'Benjamin').set(4, 'Oliver');
+});
