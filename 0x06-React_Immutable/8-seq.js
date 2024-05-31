64c366bb25776e3316d859deea595f5e063ee5d1
BENONI ESCKINDER <besckinder@gmail.com>
2024-05-31 05:31:41 +0300

    Fixing major error

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,13 +2,12 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(grades) {
   const gradesSeq = Seq(grades);
-  gradesSeq
+  const bestStudents = gradesSeq
     .filter((student) => student.score >= 70)
     .map((student) => ({
       ...student,
       firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
       lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1),
     }));
-
-  console.log(gradesSeq.toJS());
+  console.log(bestStudents.toJS());
 }
