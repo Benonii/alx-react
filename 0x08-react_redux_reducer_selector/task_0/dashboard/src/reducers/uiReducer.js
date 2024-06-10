d3b31276b32661f229b1d5c0f73ee09d5208eaec
BENONI ESCKINDER <besckinder@gmail.com>
2024-06-10 10:14:21 +0300

    Task 0

---
+++ 0x08-react_redux_reducer_selector/task_0/dashboard/src/reducers/uiReducer.js
@@ -0,0 +1,49 @@
+import * as actionTypes from '../actions/uiActionTypes';
+
+const initialState = {
+    isNotificationDrawerVisible: true,
+    isUserLoggedIn: false,
+    user: {}
+};
+
+export default function uiReducer(state=initialState, action) {
+    switch (action) {
+        case actionTypes.DISPLAY_NOTIFICATION_DRAWER: {
+            return {
+                ...state,
+                isNotificationDrawerVisible: true,
+            }
+        }
+
+        case actionTypes.HIDE_NOTIFICATION_DRAWER: {
+            return {
+                ...state,
+                isNotificationDrawerVisible: false,
+            }
+        }
+
+        case actionTypes.LOGIN_SUCCESS: {
+            return {
+                ...state,
+                isUserLoggedIn: true,
+            }
+        }
+
+        case actionTypes.LOGIN_FAILURE: {
+            return {
+                ...state,
+                isUserLoggedIn: false,
+            }
+        }
+
+        case actionTypes.LOGIN: {
+            return {
+                ...state,
+                isUserLoggedIn: false,
+            }
+        }
+
+        default:
+            return state
+    }
+}
