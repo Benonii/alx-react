10e6955e9381121bf76184e8a7fe2d70a899354e
BENONI ESCKINDER <besckinder@gmail.com>
2024-06-10 18:38:11 +0300

    Task 3

---
+++ 0x08-react_redux_reducer_selector/task_3/dashboard/src/reducers/courseReducer.js
@@ -0,0 +1,41 @@
+import * as actionTypes from '../actions/courseActionTypes';
+
+const initialState = [];
+
+export default function courseReducer(state=initialState, action) {
+    switch(action.type) {
+        case actionTypes.FETCH_COURSE_SUCCESS: {
+            const courseList = action.data;
+            for (const course of courseList) {
+                course.isSelected = false;
+            }
+
+            return courseList;
+        };
+        case actionTypes.SELECT_COURSE: {
+            const id = action.index;
+            const newCourseList = state.map((course) => {
+                if (course.id === id) {
+                    return {...course, 'isSelected': true};
+                }
+                return course;
+            });
+
+            return newCourseList;
+        };
+        case actionTypes.UNSELECT_COURSE: {
+            const id = action.index;
+            const newCourseList = state.map((course) => {
+                if (course.id === id) {
+                    return {...course, 'isSelected': false};
+                }
+                return course;
+            });
+
+            return newCourseList;
+        };
+
+        default:
+            return state;
+    }
+};
