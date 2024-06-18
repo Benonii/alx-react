c868930bde9fd069568ae49c4bee6c6e17a75a5f
BENONI ESCKINDER <besckinder@gmail.com>
2024-06-18 11:52:05 +0300

    Task 14

---
+++ 0x09-react_redux_connectors_and_providers/task_4/dashboard/src/reducers/rootReducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from '@reduxjs/toolkit'
+import courseReducer from './courseReducer';
+import notificationReducer from './notificationReducer';
+import uiReducer from './uiReducer';
+
+const rootReducer = combineReducers({
+    course: courseReducer,
+    notification: notificationReducer,
+    ui: uiReducer,
+});
+
+export { rootReducer };
